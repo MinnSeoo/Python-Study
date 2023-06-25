ad8ef61ae9814a1fca836f927bb139fa7423b4b5
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2023-06-26 00:45:38 +0900

    문제 설명 추가 및 설명 수정

--- FastCampus/Chap6/06-3. 함수 실습문제2.py
+++ FastCampus/Chap6/06-3. 함수 실습문제2.py
@@ -1,6 +1,15 @@
+# 함수 실습문제
+# "로또 예상번호 추출 프로그램" 만들기!
+# [조건]
+# 1. 로또 번호를 6개 생성
+# 2. 로또 번호는 1 ~ 45중 랜덤한 숫자이다.
+# 3. 로또 번호는 각각 다른 숫자이여야 한다.
+# 4. getRandomNumber() 함수를 사용해서 구현한다.
+
+
 import random   # random 함수를 import
 
-NumOfList = []  # random 숫자를 받아와서 저장할 list 선언
+LottOfList = []  # random 숫자를 받아와서 저장할 list 선언
 count = 0       # list에 값을 몇개 넣었는지 확인하는 변수
 
 # 함수 선언
@@ -15,10 +24,9 @@ while True:
         break       # while 문 종료
     randomNum = get_random_number()   # 그렇지 않다면 randomNum에 get_random_number 함수 호출하여 값을 저장
 
-    if randomNum not in NumOfList:  # 문제에서 중복된 값은 허용x -> 로또 리스트에 중복값이 들어가지 않았는지 확인
-        NumOfList.append(randomNum) # 현재 NumOfList 리스트의 마지막 인자로 전달된 값을 추가.
+    if randomNum not in LottOfList:  # 문제에서 중복된 값은 허용x -> 로또 리스트에 중복값이 들어가지 않았는지 확인
+        LottOfList.append(randomNum) # 현재 LottOfList 리스트의 마지막 인자로 전달된 값을 추가.
         count += 1                  # cnt 횟수 증가.
 
-NumOfList.sort()    # 숫자를 오름차순으로 정렬
-for num in NumOfList:   
-    print(NumOfList, "") 
+LottOfList.sort()    # 숫자를 오름차순으로 정렬
+print(LottOfList, "") # LottoOfList를 출력!
