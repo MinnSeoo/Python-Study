5c17d7c29fe490e721020c98625a2a5edbc306a9
MinnSeoo <ms.rry01@gmail.com>
2024-01-24 13:38:22 +0900

    ✨ :: url,views 처리 기능 구현

--- Django/Book Project1/Todo_List/My_Todo_App/views.py
+++ Django/Book Project1/Todo_List/My_Todo_App/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'My_Todo_App/index.html')        # html 파일을 user에게 보여주기 위하여 render 함수 사용
