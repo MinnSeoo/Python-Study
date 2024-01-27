e2c84a1e5b26907449aac4d0399f2407543b9909
MinnSeoo <ms.rry01@gmail.com>
2024-01-27 19:07:32 +0900

    ✨ :: user가 입력한 값 db에 저장 및 db에 저장된 값을 user에게 보여주기 기능 구현

--- Django/Book Project1/Todo_List/My_Todo_App/views.py
+++ Django/Book Project1/Todo_List/My_Todo_App/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'My_Todo_App/index.html')        # html 파일을 user에게 보여주기 위하여 render 함수 사용
+    todos = Todo.objects.all()
+    content = {'todos' : todos}
+    return render(request, 'My_Todo_App/index.html', content)        # html 파일을 user에게 보여주기 위하여 render 함수 사용
+
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("Create Todo! =>" + user_input_str)                     # user에게 해당 메시지를 출력해줌.
